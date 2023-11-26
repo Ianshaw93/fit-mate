c9ac9a94e79156e88d5c2c306bae8bd5e8947c03
Ian <ianshaw93@gmail.com>
2023-11-26 20:58:18 +0000

    attempt to fix button linking to form

--- components/Hero.js
+++ components/Hero.js
@@ -13,7 +13,7 @@ const Hero = () => {
 		router.push('/form');
 		};
   return (
-    <div id={'home'} className='w-full h-screen'>
+    <div className='w-full h-screen'>
         {/* Container */}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
           {/* <h1 className='text-4xl sm:text-6xl font-bold'>Elevate Your Fitness Business with AI:</h1> */}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,12 +14,6 @@ import white_logo from '../public/logo/fit_mate_cropped.png'
 export default function NavComponent() {
 	const [nav, setNav] = useState(false);
 	const handleClick = () => setNav(!nav); // not working??
-	const contactButtonStyle = 'text-white bg-[#9CC9B7] hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-	const contactButton = (
-		<button className={contactButtonStyle}>
-			<a href="#contact">Contact</a>
-		</button>
-		)
 
 	return (
 		<>
