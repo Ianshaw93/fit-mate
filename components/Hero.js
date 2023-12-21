5489b41fd040daf1a7bc64e402793712d5edcc12
Ian <ianshaw93@gmail.com>
2023-12-21 08:10:17 +0000

    changed offer to insta persona only

--- components/Hero.js
+++ components/Hero.js
@@ -17,12 +17,15 @@ const Hero = () => {
         {/* Container */}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
           {/* <h1 className='text-4xl sm:text-6xl font-bold'>Elevate Your Fitness Business with AI:</h1> */}
-          <h1 className='text-4xl sm:text-4xl font-bold'>Elevate Your Fitness Business with AI:</h1>
+          <h1 className='text-4xl sm:text-4xl font-bold'>More Clients, More Impact:</h1>
           {/* <h2 className='text-4xl sm:text-6xl font-bold text-[#CBD0D3]'>More Time, More Clients, More Impact</h2> */}
           {/* <h2 className='text-2xl sm:text-2xl font-bold text-[#CBD0D3]'>Professional Video Editing:</h2> */}
           {/* Unlock a new level of efficiency with our two-pronged AI solution: */}
 
-          Elevate your client interaction with our state-of-the-art automated engagement system. Tailored to your business needs, this tool effortlessly manages and nurtures leads through personalized, automated responses. Imagine having a virtual assistant who is always on, guiding your potential clients towards the right fitness solutions, all while you focus on what you do best - transforming lives.
+          Never lose a hot lead again with our automatic appointment setter/product seller inbound Instagram persona to qualify and book calls on your calendar. 
+          <p className='py-4'>
+            Tailored to your business needs, this tool effortlessly manages and nurtures leads through personalized, automated responses. 
+          </p>
           {/* <h2 className='text-2xl sm:text-2xl font-bold text-[#CBD0D3]'>Streamlined Communication:</h2>
           Boost your sales and client retention rates. Manage and automate all your messages and emails effortlessly in one place. */}
 
