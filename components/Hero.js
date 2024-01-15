150ff6005fac70c3c87bca925383f2e2aa55a6da
Ian <ianshaw93@gmail.com>
2024-01-15 17:12:22 +0000

    changed vsl

--- components/Hero.js
+++ components/Hero.js
@@ -49,7 +49,7 @@ const Hero = () => {
             className="w-full aspect-video" 
             // width="560" 
             // height="315" 
-            src="https://www.youtube.com/embed/PLhBw0xj-ig" 
+            src="https://www.youtube.com/embed/V8irrLFWV9w?si=Kz_OzhXdaJKZssa0" 
             title="YouTube video player" 
             frameBorder="0" 
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
