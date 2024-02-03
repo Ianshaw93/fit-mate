42c003e9e30ea23543e84bdbe03d6d903d637d83
Ian <ianshaw93@gmail.com>
2024-02-03 12:25:35 +0000

    updated video

--- components/Hero.js
+++ components/Hero.js
@@ -49,7 +49,7 @@ const Hero = () => {
             className="w-full aspect-video" 
             // width="560" 
             // height="315" 
-            src="https://www.youtube.com/embed/V8irrLFWV9w?si=Kz_OzhXdaJKZssa0" 
+            src="https://www.youtube.com/embed/0x-49ftzoK8?si=0j3gXLNfkZJ0v_HV" 
             title="YouTube video player" 
             frameBorder="0" 
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
